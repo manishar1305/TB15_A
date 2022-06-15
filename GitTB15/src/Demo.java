f0d9b9ee6bd1d6a63435b7cfbbc887f0b07a97dc
manishar1305 <107458525+manishar1305@users.noreply.github.com>
2022-06-15 11:36:05 +0530

    by git

--- GitTB15/src/Demo.java
+++ GitTB15/src/Demo.java
@@ -6,6 +6,12 @@ public class Demo {
 		System.out.println("m1 method");
 
 	}
+	
+	public void m2()
+	{
+		System.out.println("m2 method");
+
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("GIT DEMO SELENIUM");
